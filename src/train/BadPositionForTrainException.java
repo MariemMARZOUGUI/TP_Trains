e7abf5cebfec957b90ac37029bcff2af01821f0b
Ihebsmai <146442711+Ihebsmai@users.noreply.github.com>
2024-01-30 17:28:52 +0100

    Update BadPositionForTrainException.java

--- src/train/BadPositionForTrainException.java
+++ src/train/BadPositionForTrainException.java
@@ -1,13 +1,19 @@
 package train;
 
+// La classe BadPositionForTrainException qui étend la classe Exception
 public class BadPositionForTrainException extends Exception {
-	private static final long serialVersionUID = 1L;
-	
-	public BadPositionForTrainException(String name){
-		super(name);
-	}
-	
-	public String getMessage(){
-		return super.getMessage();
-	}
+    // Déclaration d'un identifiant unique pour la sérialisation de la classe
+    private static final long serialVersionUID = 1L;
+    
+    // Constructeur de la classe prenant un argument de type String
+    public BadPositionForTrainException(String name){
+        // Appel du constructeur de la classe mère (Exception) avec le message d'erreur
+        super(name);
+    }
+    
+    // Cette méthode retourne le message d'erreur associé à l'exception
+    public String getMessage(){
+        // Appel de la méthode getMessage() de la classe mère (Exception)
+        return super.getMessage();
+    }
 }
